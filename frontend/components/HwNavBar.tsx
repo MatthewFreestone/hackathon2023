2ed6a23166f44ad3c2531bec2e1875d56bfea1fc
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-02-11 17:03:43 -0600

    basic layout start

---
+++ frontend/components/HwNavBar.tsx
@@ -0,0 +1,13 @@
+
+const HwNavBar = () => {
+    return (<>
+        <nav className="w-full bg-gray-100 flex flex-row fixed text-black">
+            <div className="mx-2">
+                <a href="/" className="text-3xl font-bold text-black">VP</a>
+            </div>
+            <p></p>
+        </nav>
+    </>)
+}
+
+export default HwNavBar;
