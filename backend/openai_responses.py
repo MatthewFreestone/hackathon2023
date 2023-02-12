e60ff3f6e0a11657ee3cda6f0b34da225b3c286e
Caden Garrett <garrettcs11@gmail.com>
2023-02-12 07:31:57 -0600

    Added harder day prompt for openAI

--- backend/openai_responses.py
+++ backend/openai_responses.py
@@ -16,10 +16,15 @@ def get_encourage_message():
     return response["choices"][0]["text"]
 
 
-def get_congrats_message():
+def get_congrats_message(hard_day=False):
+
+    prompt_string = "Give me a short congratulatory message about getting my homework done today"
+    if hard_day:
+        prompt_string += ". Today was the toughest day of homework I have had."
+
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Give me a short congratulatory message about getting my homework done today",
+        prompt=prompt_string,
         max_tokens=70,
         temperature=random.random() * 2
     )
