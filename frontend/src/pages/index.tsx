edb39fcf3f86fba1cc1094d22615d34326de8039
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-02-11 19:21:58 -0600

    added a navbar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -15,14 +15,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <HwNavBar />
-        <div className="mt-10">
-          <h1 className="text-3xl font-bold text-white">Vacation Planner</h1>
-        </div>
-        <div className='border-solid border-red'>
-          <input type="text" placeholder="Search" />
-        </div>
+        <body className={styles.main}>
+          <div className="mt-10">
+            <h1 className="text-3xl font-bold text-white">Vacation Planner</h1>
+          </div>
+          <div className='border-solid border-red'>
+            <input type="text" placeholder="Search" />
+          </div>
+        </body>
       </main>
     </>
   )
